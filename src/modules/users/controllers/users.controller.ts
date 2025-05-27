3275c00b3b61262efbc3ee48fd105cc1ef119f4f
v.kragel <v.kragel@softteco.com>
2025-05-27 23:24:12 +0300

    feat: add users controllers

---
+++ src/modules/users/controllers/users.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from '../services';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+}
