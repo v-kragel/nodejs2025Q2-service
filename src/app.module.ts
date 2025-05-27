5bf738ab8f844fb06184ac95cc8c904110d219e7
v.kragel <v.kragel@softteco.com>
2025-05-27 23:46:22 +0300

    feat: add tracks module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users';
 import { ArtistsModule } from './modules/artists';
+import { TracksModule } from './modules/tracks';
 
 @Module({
-  imports: [UsersModule, ArtistsModule],
+  imports: [UsersModule, ArtistsModule, TracksModule],
   controllers: [AppController],
   providers: [AppService],
 })
