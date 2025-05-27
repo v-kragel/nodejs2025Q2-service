3d2d4589a00581eccbf5ee0e9ce0f479cb27aee7
v.kragel <v.kragel@softteco.com>
2025-05-27 23:34:12 +0300

    feat: add artists module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './modules/users/users.module';
+import { UsersModule } from './modules/users';
+import { ArtistsModule } from './modules/artists';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ArtistsModule],
   controllers: [AppController],
   providers: [AppService],
 })
